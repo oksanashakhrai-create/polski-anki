0bc8e6282f4bc8db4f8feceba646c7e8cf3669ca
Ksu Shine <ksushine@Ksus-MacBook-Pro.local>
2026-06-14 22:02:43 +0200

    Compact mobile layout, rename app to PL-ENG, merge categories to Микс, shorten blend card

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'polski-v3';
+const CACHE = 'polski-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
