0a8a77dde4e48651eea214be9f6aa445764e2f20
Ksu Shine <ksushine@Ksus-MacBook-Pro.local>
2026-06-14 21:54:21 +0200

    Bump service worker cache to v3 to force update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'polski-v2';
+const CACHE = 'polski-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
