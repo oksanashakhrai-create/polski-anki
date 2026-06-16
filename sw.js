54ad236a2c446a05358bb34800ef0dc6f067a08c
Ksu Shine <ksushine@Ksus-MacBook-Pro.local>
2026-06-16 16:45:41 +0200

    Add 21 Polish cards, edit 5 English cards, fix card height consistency

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'polski-v12';
+const CACHE = 'polski-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
